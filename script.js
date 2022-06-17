1b58371fdd0b94d6d87924ea6f3b7d231865a579
John Servo <jaservo@up.edu.ph>
2022-06-17 17:34:45 +0800

    added height on bottom result

--- script.js
+++ script.js
@@ -19,25 +19,33 @@ const clearResults = result => {
     result.removeChild(result.firstChild);
 };
 
-const result = document.getElementById("result")
-const pad = document.getElementById("buttons");
-const padContents = pad.querySelectorAll("button");
+const buttons = document.getElementById("buttons");
+const pad = buttons.querySelectorAll("button");
 
-let val1 = "";
-let val2 = "";
-padContents.forEach(item => {
+const topResult = document.getElementById("top");
+const bottomResult = document.getElementById("bottom");
+
+let op = "";
+let displayValue = "";
+pad.forEach(item => {
     item.addEventListener("click", () => {
-        if (item.id === "clear") {
-            clearResults(result);
-            return;
-        } else if (item.id >= "0" && item.id <= "9") {
-            result.textContent += item.id;
-            val1 = result.textContent;
-        } else if (item.id === "delete") {
-            result.textContent = result.textContent.slice(0, result.textContent.length - 1);
-        } else {
-            
+        if(item.id >= "0" && item.id <= "9") {
+            bottomResult.textContent += item.id;
+        } else if(item.id == "add" || item.id == "subtr" || item.id == "mult" || item.id == "divide") {
+            op = item.id;
+            displayValue = bottomResult.textContent;
+            topResult.textContent = `${displayValue} ${item.textContent}`;
+            clearResults(bottomResult);
         }
-            
     });
 });
+
+
+// Get inputs from user
+// If user inputs numbers, collect it and store it and output it
+// If user inputs operators 
+    // collect it
+    // output the prev. input on top with the operator
+    // Collect input again but store it in a different variable
+        // Clear the previous output after inputting the new number
+// When user inputs =, then show result

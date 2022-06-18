c109269081c58be104d095ce68885152a222df17
John Servo <jaservo@up.edu.ph>
2022-06-18 11:26:29 +0800

    added a few comments

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// NOT CLEAN YET WILL FIX LATER LMAO
+/*
+    > Code is not "functionized"
+    > When tapping on operators multiple times, it operates on the current number
+        > If there is an active operator, it must not operate on the current number
+        > It must change from the old operator to the operator tapped
+*/
+
+
 // Functions for math operations
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -32,12 +41,13 @@ pad.forEach(item => {
     item.addEventListener("click", () => {
         // Checks if the button tapped is a number
         if(item.id >= "0" && item.id <= "9" || item.id == "dot") {
-            if (flag == 1) bottomResult.textContent = "";
+            if (flag == 1) bottomResult.textContent = ""; // Clears the bottom when inputting new number
             flag = 0;
             bottomResult.textContent += item.textContent;
         // Checks if the button tapped is an operator
         } else if(item.id == "add" || item.id == "subtr" || item.id == "mult" || item.id == "divide") {
-            if(!bottomResult.textContent) return;
+            if(!bottomResult.textContent) return; // If there are no inputs yet, ignore
+            // If the user wants to continue operating without pressing =
             else if(displayValue) bottomResult.textContent = operate(displayValue, bottomResult.textContent, op);
             displayValue = bottomResult.textContent;
             op = item.id;
@@ -45,6 +55,7 @@ pad.forEach(item => {
             flag = 1;
         // Checks if the button tapped is equals
         } else if(item.id == "equals") {
+            // If the user didn't enter anything/entered an incomplete sequence
             if(displayValue == "" && bottomResult.textContent == "") {
                 bottomResult.textContent = "Enter numbers pls";
                 flag = 1;
@@ -56,11 +67,11 @@ pad.forEach(item => {
             }
             const value = operate(displayValue, bottomResult.textContent, op);
 
+            // If the user divides by 0
             if(value == "Infinity") {
                 bottomResult.textContent = "lmao";
                 flag = 1;
-            }
-            else bottomResult.textContent = value;
+            } else bottomResult.textContent = value;
             
             topResult.textContent = "";
             displayValue = "";
